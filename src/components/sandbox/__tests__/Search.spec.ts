a301666ffd0c702061bcd064d264c722a8051de9
Marrion Lulu <marrion91300@gmail.com>
2021-07-13 19:50:42 -0700

    Adding search component specs

---
+++ src/components/sandbox/__tests__/Search.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import Search from '@/components/sandbox/Search.vue'
+
+describe('Search specs', () => {
+  let wrapper: any
+
+  beforeEach(() => (wrapper = shallowMount(Search)))
+
+  it('renders a search input.', () => {
+    expect(wrapper.get('form').exists()).toBe(true)
+  })
+
+  it('performs a search.', async () => {
+    await wrapper.get("[data-test='search']").setValue('google search')
+    await wrapper.get("[data-test='submit']").trigger('submit')
+
+    expect(wrapper.emitted('on-search')[0]).toEqual(['google search'])
+  })
+})
