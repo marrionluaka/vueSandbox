8314fa76d9e6ec6b0ef9e2d424851aa33d8ea1ac
Marrion Lulu <marrion91300@gmail.com>
2021-08-10 09:04:10 -0700

    Updating book-api spec

--- src/api/book-api.spec.ts
+++ src/api/book-api.spec.ts
@@ -3,14 +3,23 @@ import bookApi from './book-api'
 describe('bookApi specs', () => {
   it('throws an error when get all books fails.', async () => {
     const api = bookApi({ get: jest.fn().mockRejectedValue(null) })
-    await expect(api.getAllBooks()).rejects.toThrow('Cannot execute getAllBooks API')
+    await expect(api.getBooks()).rejects.toThrow('Cannot execute getBooks API')
   })
 
   it('returns books', async () => {
     const books = [{ title: 'some-book' }]
     const api = bookApi({ get: jest.fn().mockResolvedValue({ data: books }) })
 
-    const data = await api.getAllBooks()
+    const data = await api.getBooks()
+
+    expect(data).toEqual(books)
+  })
+
+  it('returns books for a specified search term', async () => {
+    const books = [{ title: 'Elon Tesla...' }]
+    const api = bookApi({ get: jest.fn().mockResolvedValue({ data: books }) })
+
+    const data = await api.getBooks('Elon')
 
     expect(data).toEqual(books)
   })
