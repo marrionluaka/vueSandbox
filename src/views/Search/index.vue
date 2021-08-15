af31888bfb2a7aefbfb64b8bf8e8ea663f17bcab
Marrion Lulu <marrion91300@gmail.com>
2021-08-14 21:25:34 -0700

    Adding accordion to search page

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -23,17 +23,24 @@
             p.text-sm  Historical
             ChevronIconRight.transform.scale-75.text-gray-500
 
-  nav.col-span-full.px-5(class="lg:col-span-3")
+  nav.col-span-full(class="lg:col-span-3")
     ul(data-test="search-page-categories")
       li(v-for="category in categories" :key="category.id")
-        p.text-2xl.font-bold.capitalize {{ category.name }}
-        ul
-          li(v-for="option in category.options" :key="option")
-            | {{ option }}
+        Accordion(:isOpen="true")
+          template(#title)
+            AccordionTitle(@click="currentPanel = category.id")
+              .flex.justify-between.py-2
+                p.text-xl.font-bold.capitalize {{ category.name }}
+                ChevronIcon.transition-transform.ease-in-out.transform.scale-75(:class="[isOpen(category.id) ? 'rotate-0' : 'rotate-90']")
+
+          AccordionPanel(:isOpen="isOpen(category.id)")
+            ul.h-72.overflow-y-scroll
+              li(v-for="option in category.options" :key="option")
+                Checkbox(:id="option" :name="option" :value="option") {{ option }}
 
   main.col-span-full.px-5(class="lg:col-span-9")
     div(data-test="search-page-results")
-      ul(class="md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-5")
+      ul(class="md:grid sm:grid-cols-2 lg:grid-cols-3 md:gap-x-5")
         li.py-2(v-for="result in searchResults" :key="result.id")
           .aspect-w-3.aspect-h-2
             img.object-cover.shadow-lg.rounded-lg(:src='result.img' alt='')
@@ -48,17 +55,35 @@ import { bookService } from '../../api'
 
 import { IBook } from '@/entities/book.entity'
 import { ICategory } from '@/entities/category.entity'
+
 import Search from '../../components/sandbox/Search/Search.vue'
-import { ChevronIconRight } from '../../components/sandbox/shared'
+import { ChevronIconRight, ChevronIcon } from '../../components/sandbox/shared'
 import SearchItem from '../../components/sandbox/Search/SearchItem.vue'
 import SearchAction from '../../components/sandbox/Search/SearchAction.vue'
+
+import Accordion from '../../components/sandbox/Accordion/Accordion.vue'
+import AccordionPanel from '../../components/sandbox/Accordion/AccordionPanel.vue'
+import AccordionTitle from '../../components/sandbox/Accordion/AccordionTitle.vue'
+
+import Checkbox from '../../components/sandbox/Checkbox/Checkbox.vue'
 
 export default defineComponent({
   name: 'SearchPage',
 
-  components: { Search, SearchItem, SearchAction, ChevronIconRight },
+  components: {
+    Search,
+    SearchItem,
+    SearchAction,
+    ChevronIcon,
+    ChevronIconRight,
+    Accordion,
+    AccordionPanel,
+    AccordionTitle,
+    Checkbox
+  },
 
   setup() {
+    const currentPanel: Ref<number> = ref(0)
     const searchResults: Ref<IBook[]> = ref([])
     const categories: Ref<ICategory[]> = ref([])
     const suggestedResults: Ref<IBook[]> = ref([])
@@ -68,6 +93,7 @@ export default defineComponent({
       await getCategories()
     })
 
+    const isOpen = (x: number) => x === currentPanel.value
     const onSearch = _getBooks((books: IBook[]) => (searchResults.value = books))
     const onKeydown = _getBooks((books: IBook[]) => (suggestedResults.value = books.slice(0, 3)))
     const getCategories = async () => (categories.value = await bookService.getCategories())
@@ -79,7 +105,15 @@ export default defineComponent({
       }
     }
 
-    return { categories, searchResults, suggestedResults, onSearch, onKeydown }
+    return {
+      categories,
+      currentPanel,
+      searchResults,
+      suggestedResults,
+      isOpen,
+      onSearch,
+      onKeydown
+    }
   }
 })
 </script>
