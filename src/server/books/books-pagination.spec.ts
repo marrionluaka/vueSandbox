1a66e47ae2e4ee4f7edd0130b8bcbf32cd1b3685
Marrion Lulu <marrion91300@gmail.com>
2021-08-24 18:51:26 -0700

    Improving pagination logic

---
+++ src/server/books/books-pagination.spec.ts
@@ -0,0 +1,20 @@
+import { range } from 'ramda'
+import { paginate } from './books-pagination'
+
+describe('books pagination', () => {
+  it.each`
+    page | expected
+    ${1} | ${{ count: 30, next: 2, prev: 0, results: range(5, 10) }}
+    ${2} | ${{ count: 30, next: 3, prev: 1, results: range(10, 15) }}
+    ${3} | ${{ count: 30, next: 4, prev: 2, results: range(15, 20) }}
+    ${4} | ${{ count: 30, next: 5, prev: 3, results: range(20, 25) }}
+    ${5} | ${{ count: 30, next: 0, prev: 4, results: range(25, 30) }}
+  `('paginates a given list', ({ page, expected }) => {
+    const items = range(0, 30)
+    const limit = 5
+
+    const actual = paginate(limit, page, items)
+
+    expect(actual).toEqual(expected)
+  })
+})
--- src/server/books/index.ts
+++ src/server/books/index.ts
@@ -1,15 +1,15 @@
-import { pipe, pluck, uniq, forEach, slice, curry, pick } from 'ramda'
+import { pipe, pluck, uniq, forEach, curry, pick, __, filter, flip, tap } from 'ramda'
 // @ts-ignore
 import { Factory, Model, hasMany } from 'miragejs'
 // @ts-ignore
 import faker from 'faker'
 import booksDb from './books-db.json'
+import { paginate } from './books-pagination'
 import { IBook } from '@/entities/book.entity'
 import { ICategory } from '@/entities/category.entity'
 import { getBooks, bookCategories } from './books-utils'
 
 export default function(server: any) {
-  const limit = curry((lim, arr) => slice(0, lim, arr))
   const getRating = (min: number, max: number): number => parseFloat((Math.random() * (max - min + 1) + min).toFixed(1))
   const getUniqCategory = (books: IBook[], categoryName: string) => pipe<any, any, IBook[]>(pluck(categoryName), uniq)(books)
   const getCategoryType = (categoryName: string) => (['volume_sales', 'publication_date'].includes(categoryName) ? 'range' : 'selection')
@@ -49,9 +49,15 @@ export default function(server: any) {
     routes() {
       this.namespace = 'api'
 
-      this.get('books', ({ books }: any, req: any) => {
-        const searchQuery = pick(bookCategories.concat('q'), req.queryParams)
-        return limit(25, books.where(getBooks(searchQuery)))
+      this.get('books', ({ db }: any, req: any) => {
+        const reversedFilter: any = flip(filter)
+
+        return pipe(
+          pick(bookCategories.concat('q')),
+          getBooks,
+          reversedFilter(db.books),
+          paginate(+req.queryParams?.limit, +req.queryParams?.page)
+        )(req.queryParams)
       })
       this.get('books/categories', (schema: any) => {
         return schema.categories.all()
