97db2b4ff4ad348efa3cb60944a6715cd18c5e2f
Marrion Lulu <marrion91300@gmail.com>
2021-12-04 14:15:48 -0800

    Rearranging import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 import './assets/index.css'
+import App from './App.vue'
+import store from './store'
+import router from './router'
 import server from './features/api/backend'
 
 server()
